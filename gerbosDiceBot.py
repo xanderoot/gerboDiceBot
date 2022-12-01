aab570dafa18dcd1e77ccaa13bf7b2e93d4d7308
xanderoot <45837051+xanderoot@users.noreply.github.com>
2022-11-30 19:49:49 -0800

    V1.0 Release

---
+++ gerbosDiceBot.py
@@ -0,0 +1,190 @@
+
+import discord
+import random
+import time
+import re
+import requests
+import math
+
+def diceDecode(diceInput):
+    characters = []
+    splitValue = diceInput.split('d')
+    #print(splitValue)
+    return splitValue
+    #for letter in diceInput:
+        #characters.append(letter)
+
+def randomJsonRequest(diceNumber,diceType,apiKey):
+    jsonResponse = requests.post('https://api.random.org/json-rpc/4/invoke', json={
+    "jsonrpc": "2.0",
+    "method": "generateIntegers",
+    "params": {
+        "apiKey": apiKey,
+        "n": diceNumber,
+        "min": 1,
+        "max": diceType,
+        "base": 10
+    },
+    "id": 19185
+    })
+    #print(f"Status Code: {jsonResponse.status_code}, Response: {jsonResponse.json()}")
+    return jsonResponse.json()['result']['random']['data']
+
+######################################################################################################
+runningOnPi = 1 #depending on the python version it needs the full path. on my main pc and the pi, it works with just the file name, but my laptop needs the full path. set to one for rapid debugging
+#runningOnPi = int(input('Is this running on the pi? 1 for yes 0 for no.'))
+if runningOnPi == 1:
+    x = open("tokens.env", 'rt')
+else:
+    x = open("Python/gerboDiceBot/tokens.env", 'rt')
+dotenv = x.readlines(0)
+discordToken = dotenv[0]  #go to discords developer portal and create a bot and get your token there.
+randomToken = dotenv[1] #go to random.org and get your token after creating an api key. remember, non commercial unless you pay.
+
+x.close()
+
+intents = discord.Intents.default()
+intents.message_content = True
+client = discord.Client(intents=intents)
+
+######################################################################################################
+#main command stuffs
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+@client.event
+async def on_message(message):
+
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
+
+    if message.content.startswith('$help'):
+        await message.channel.send('Welcome to my dice shop. Currently underconstruction.')
+
+
+    if message.content.startswith('$roll'):
+        rolled20 = False #just a bunch of variables :)
+        rolled1 = False
+        nat20Count = 0
+        splitString = message.content.split() #splits string so we can parse the words
+        splitString.append('') #adds a blank string for debugging. I used it in another program and it got left over, its useful so im leaving it
+        userChoice = splitString[1].lower() #sanitize your inputs
+        values = diceDecode(userChoice) #turns 2d20 into ['2','20']
+        diceNumber = values[0]
+        diceType = values[1]
+        roll = randomJsonRequest(diceNumber,diceType,randomToken)
+        await message.channel.send(roll) #roll them bones
+
+        total = 0
+        for each in roll: #counts the rolls
+            total += int(each)
+        if int(diceType) == 20: #checks if the user requested d20s
+            if 20 in roll: rolled20 = True
+            if 1 in roll: rolled1 = True
+            if rolled20 == True and rolled1 == True:
+                await message.channel.send('Sure hope that was advantage.')
+            if rolled20 == True: await message.channel.send('Nat 20 :)')
+            if rolled1 == True: await message.channel.send('Nat 1 :(')
+            for each in roll:
+                if int(each) == 20:
+                    nat20Count += 1
+            if nat20Count >= 2:
+                await message.channel.send('Double crit :)')
+######################################################################################################
+#begin d20 auditing
+            if runningOnPi == 1:
+                x = open("d20auditor.txt", 'rt')
+            else:
+                x = open("Python/gerboDiceBot/d20auditor.txt", 'rt')        
+
+            playerTempScore = x.readlines() #loads the saved data
+            x.close()
+            
+            tempList = []
+
+            for each in playerTempScore: #turns the string '0\n' into 0. this was a bitch to code, tuns out \n is one character in a string.
+                each = each.rstrip(each[-1]) #it is important to remove the last character in the string.
+                tempList.append(int(each))
+
+            d20Index = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20] #im sure there is an easier way to make these especially if i plan on doing more dice
+
+            copyOfRoll = [] #separates copyOfRoll from roll so we can use either without affecting the other
+            for each in roll:
+                copyOfRoll.append(each)
+
+            print(tempList)
+
+            for each in roll: #updates the saved data via temp data. I dont like this being hardcoded
+                currentDieRoll = copyOfRoll[0]
+                currentIndex = d20Index.index(currentDieRoll)
+                tempList[currentIndex] += 1
+                copyOfRoll.pop(0)
+
+            strList = []
+
+            for each in tempList:
+                tempValue = str(each)
+                stringWithNewLine = tempValue + '\n'
+                strList.append(stringWithNewLine)
+
+            if runningOnPi == 1: #must be opened in write mode
+                x = open('d20auditor.txt','w')
+            else:
+                x = open('Python/gerboDiceBot/d20auditor.txt','w')
+            x.close
+
+            print(strList)
+            x.writelines(strList)
+
+######################################################################################################
+    
+    if message.content.startswith('$audit'): 
+        if runningOnPi == 1:
+            x = open("d20auditor.txt", 'rt')
+        else:
+            x = open("Python/gerboDiceBot/d20auditor.txt", 'rt')
+
+        auditList = []
+
+        for each in x: #turns the string '0\n' into 0. this was a bitch to code, tuns out \n is one character in a string.
+            each = each.rstrip(each[-1]) #it is important to remove the last character in the string.
+            auditList.append(int(each))
+
+        await message.channel.send(auditList)
+        #calculating standard deviation and variance.
+        stdDeviation = 0
+        totalOfAudit = sum(auditList)
+        meanOfAudit = totalOfAudit / len(auditList)
+        
+        mathedAuditList = []
+
+        for each in auditList:
+            each -= meanOfAudit
+            each *= each
+            mathedAuditList.append(each)
+
+        sumOfMathedAuditList = sum(mathedAuditList)
+        variance = sumOfMathedAuditList / (len(auditList) - 1)
+        sqrOfVariance = math.sqrt(variance) #not using due to only needing the variance value
+        #await message.channel.send('Variance of all d20 rolls made by this bot is: {:.2f}.\nA good variance is between 10 and 30. The lower the better.'.format(variance))
+        highestValue = 0
+        lowestValue = 0
+        for each in auditList:
+            if each >= highestValue:
+                highestValue = each
+        for each in auditList:
+            if each <= highestValue:
+                lowestValue = each
+        await message.channel.send('The highest result count is {}.\nThe lowest result count is {}.'.format(highestValue,lowestValue))
+        n = 1
+        allRolls = []
+        for each in auditList:
+            for x in range(each):
+                allRolls.append(n)
+            n += 1
+        averageRoll = sum(allRolls) / len(allRolls)
+        await message.channel.send('Calculating based off of {} rolls.\nAverage of all rolls is {:.2f}.'.format(sum(auditList),averageRoll))
+            
+client.run(discordToken)
